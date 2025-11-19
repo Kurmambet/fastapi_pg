646de430aa52accfb53ceaea5f322fc1c0e3ab29
Petr <pitr.vishnyakovv.05@gmail.com>
2025-11-19 17:09:48 +0500

    cleaned orm work

--- src/models.py
+++ src/models.py
@@ -5,25 +5,6 @@ from sqlalchemy import ForeignKey, Table, Column, Integer, String, MetaData, fun
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base, str_256
 
-# metadata_obj = MetaData() # данные о всех таблицах на стороне приложения
-
-
-
-# # объявление в императивном стиле
-# workers_table = Table(
-#     "workers",
-#     metadata_obj,
-#     Column("id", Integer, primary_key=True),
-#     Column("username", String),
-# ) 
-
-
-
-
-
-
-
-
 
 
 
@@ -43,13 +24,12 @@ updated_at = Annotated[datetime.datetime, mapped_column(
 
 
 
-# объявление в декларативном стиле
+
 class WorkersOrm(Base):
     __tablename__ = "workers"
 
-    # id: Mapped[int] = mapped_column(primary_key=True)
     id: Mapped[intpk]
-    username: Mapped[str]                               # можно прям так оставить
+    username: Mapped[str]                              
 
 
 
@@ -58,38 +38,16 @@ class Workload(enum.Enum):
     parttime = "parttime"
     fulltime = "fulltime"
 
+
+
+
 class ResumesOrm(Base):
     __tablename__ = "resumes"
 
     id: Mapped[intpk]
-
-    # title: Mapped[str] = mapped_column(String(256))
-    title: Mapped[str_256] # сами создали этот тип данных
-
-    # compensation: Mapped[int] = mapped_column(nullable=True)
-    # compensation: Mapped[Optional[int]]
+    title: Mapped[str_256] 
     compensation: Mapped[int | None]
-
     workload: Mapped[Workload]
-
-    # worker_id: Mapped[int] = mapped_column(ForeignKey(WorkersOrm.id)) - так не надо, но можно
-    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE")) # SET NULL, но тогда надо Optional[int]
-
-    # created_at: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
-    # created_at: Mapped[datetime.datetime] = mapped_column(
-    #   default=datetime.timezone.utc
-    #   ) # - на стороне нашего приложения дата формируется
-
-    # created_at: Mapped[datetime.datetime] = mapped_column(
-    #     server_default=text("TIMEZONE('utc', now())")
-    #     ) # - на стороне бд дата формируется
-    
-
-    # updated_at: Mapped[datetime.datetime] = mapped_column(
-    #     server_default=text("TIMEZONE('utc', now())"), 
-    #     onupdate=datetime.timezone.utc
-    #     )
-
-
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE"))
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
--- src/queries/orm.py
+++ src/queries/orm.py
@@ -3,14 +3,6 @@ from database import sync_engine, async_engine, session_factory, async_session_f
 from models import WorkersOrm
 
 
-# def create_tables():
-#     metadata_obj.drop_all(sync_engine)     # from models import metadata_obj
-#     sync_engine.echo = False
-#     metadata_obj.create_all(sync_engine)    
-#     sync_engine.echo = True
-
-
-
 def create_tables():
     Base.metadata.drop_all(sync_engine)     
     sync_engine.echo = True
@@ -22,8 +14,6 @@ def insert_data():
     with session_factory() as session:
         worker_bobr = WorkersOrm(username="Bobr")
         worker_volk = WorkersOrm(username="Volk")
-        # session.add(worker_bobr)
-        # session.add(worker_volk)
         session.add_all([worker_bobr, worker_volk])
         session.commit()
 
@@ -33,8 +23,5 @@ async def insert_data():
     async with async_session_factory() as session:
         worker_bobr = WorkersOrm(username="Bobr")
         worker_volk = WorkersOrm(username="Volk")
-        # session.add(worker_bobr)
-        # session.add(worker_volk)
-        session.add_all([worker_bobr, worker_volk]) # еще никуда ничего не отправляет -> не нужен await
-                                                    # хранится в оперативе в sqlalchemy
-        await session.commit()      # а вот тут уже идет запись в бд
+        session.add_all([worker_bobr, worker_volk])                   
+        await session.commit()      
